0a897793072a4d15d97286b62ca80c1a04ea7f07
Pettijohn <pettijohn@gmail.com>
2022-01-24 14:53:41 -0800

    Version number

--- pack.ps1
+++ pack.ps1
@@ -1,6 +1,6 @@
 # two digit year, 2 digit weeknum, 2 digit hour, 2 digit mintu
 #$versionNum = get-date -UFormat %y.%V.%H.%M
-$versionNum = get-date -UFormat %y.%j.%H.%m
+$versionNum = get-date -UFormat %y.%j.%H.%M
 $projPath = ".\src\TaskSchedulerEngine.csproj"
 $csproj = [xml] (get-content $projPath)
 $csproj.Project.PropertyGroup.PackageVersion = $versionNum
