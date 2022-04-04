31dca6e791df051a43114eaed76b4c99b938fe05
Pettijohn <pettijohn@gmail.com>
2022-04-04 15:39:07 -0700

    3 place semantic version compatibility.

--- pack.ps1
+++ pack.ps1
@@ -1,6 +1,12 @@
 # two digit year, 2 digit weeknum, 2 digit hour, 2 digit mintu
 #$versionNum = get-date -UFormat %y.%V.%H.%M
-$versionNum = get-date -UFormat %y.%j.%H.%M
+$hour = get-date -AsUTC -UFormat %H
+$min = get-date -AsUTC -UFormat %M
+$minOfDay = ([Int32]$hour) * 60 + ([Int32]$min)
+$versionNum = (get-date -AsUTC -UFormat %y.%j) + "." + $minOfDay
+
+$versionNum;
+
 $projPath = ".\src\TaskSchedulerEngine.csproj"
 $csproj = [xml] (get-content $projPath)
 $csproj.Project.PropertyGroup.PackageVersion = $versionNum
