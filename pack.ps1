3fa53d6c30720410c2f57648ab02c70b288dd602
pettijohn <632844+pettijohn@users.noreply.github.com>
2023-06-13 13:28:52 -0700

    Regex source generation; cross target .NET 7

--- pack.ps1
+++ pack.ps1
@@ -7,15 +7,16 @@ $min = get-date -AsUTC -UFormat %M
 $minOfDay = ([Int32]$hour) * 60 + ([Int32]$min)
 $versionNum = (get-date -AsUTC -UFormat %y.%j) + "." + $minOfDay
 
-$versionNum;
 
 $projPath = get-item ".\src\TaskSchedulerEngine.csproj"
 $csproj = [xml] (get-content $projPath)
 $csproj.Project.PropertyGroup.PackageVersion = $versionNum
 $csProj.Save($projPath)
 
+Read-Host -Prompt $versionNum;
+
 $apiKey = get-content .nuget
 dotnet pack -c Release -o .\out\
-dotnet nuget push --source https://api.nuget.org/v3/index.json --api-key $apiKey ".\out\TaskSchedulerEngine.{$versionNum}.nupkg"
+dotnet nuget push --source https://api.nuget.org/v3/index.json --api-key $apiKey ".\out\TaskSchedulerEngine.${versionNum}.nupkg"
 
 git tag $versionNum
